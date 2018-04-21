324d6d7cb9481eca98d067ec3b78efea73c51b4f
Hannah Carnes <hecarnes@gmail.com>
2018-04-21 09:35:00 -0400

    add hero service observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES
+  //of(HEROES) returns an Observable<Hero[]> that emits a single value, the array of mock heroes.
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
-
-
