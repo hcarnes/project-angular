1e8913299f2d3b01698dbd1bd7bda15e141b4534
Hannah Carnes <hecarnes@gmail.com>
2018-04-22 19:35:21 -0400

    add path 'heroes' to show HeroesComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// RouterModule makes router directives available for use in the AppModule components that will need them.
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes: Routes= [
+  { path: 'heroes', component: HeroesComponent }
+];
+
+// RouterModule makes router directives available for use in the AppModule components that will need them
 @NgModule({
   exports: [ RouterModule ]
 })
