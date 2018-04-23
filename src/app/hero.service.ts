1f0821d814ec4477eaebf1e39c3c195fa9fe2999
Hannah Carnes <hannah@unionrealtime.com>
2018-04-23 17:02:51 -0400

    wip routing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,7 +5,7 @@ import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { MessageService } from './message.service'
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
@@ -16,5 +16,11 @@ export class HeroService {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
